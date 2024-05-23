3744f263f1343b2a835a6dada4ec68c3bf38ae00
Sam Ly <sam@samly.me>
2024-05-23 15:37:16 -0700

    feat: create contants.h

---
+++ include/utils/constants.h
@@ -0,0 +1,19 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+// default port if user doesn't provide one
+#define DEFAULT_PORT 69420
+
+// max number of chatrooms the server can handle at once
+#define MAX_CHATROOMS 16
+
+// max number of clients per chatroom
+#define MAX_CLIENTS 16
+
+// max character length of user name
+#define MAX_NAME_LENGTH 16
+
+// max character length of message
+#define MAX_MESSAGE_LENGTH 256
+
+#endif
