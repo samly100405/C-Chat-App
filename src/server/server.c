dda4dab231b75c4ab4fa93c46b4df5b5cfd575cd
Sam Ly <sam@samly.me>
2024-05-23 14:56:15 -0700

    feat: hello world from server

---
+++ src/server/server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+    puts("HELLO WORLD!");
+}
